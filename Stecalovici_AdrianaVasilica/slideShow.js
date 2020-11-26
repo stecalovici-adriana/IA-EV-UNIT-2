cc7ee8bdddd4e87b8860f405e525bcf7efdbef5a
stecalovici-adriana <73295028+stecalovici-adriana@users.noreply.github.com>
2020-11-26 13:31:58 +0200

    Add files via upload

---
+++ Stecalovici_AdrianaVasilica/slideShow.js
@@ -0,0 +1,18 @@
+var slideIndex = 0;
+
+
+function slideShow()	{
+	
+	var i;
+	var slides = document.getElementsByClassName("mySlides");
+	
+	for (i = 0; i < slides.length; i++)	{
+		slides[i].style.display = "none";
+	}
+	slideIndex++;
+	if (slideIndex > slides.length) {slideIndex = 1; }
+	
+	slides[slideIndex-1].style.display = "block";
+	
+	setTimeout(slideShow, 3000); // Change image every 3 seconds
+}
